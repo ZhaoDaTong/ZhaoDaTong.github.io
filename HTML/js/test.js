5c7c081c23ba4dc1cd818185245ecd74a3f5ff54
ZhaoDaTong <xidilizhao@gmail.com>
2016-09-28 15:50:24 +0800

    no message

---
+++ HTML/js/test.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+	$("button").click(function(){
+	$.getJSON("https://www.travelchinaguide.com/inc/app/chinaTours/public/config.json",function(data, status){
+		alert(status);
+		alert(data);
+	});
+//	$.get()
+         alert("kkk");
+    })
+})
+
